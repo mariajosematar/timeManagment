f62010eeadca28da3b5e9efb3420816916786489
María Jose <80407624+mariajosematar@users.noreply.github.com>
2023-08-29 13:43:08 +0200

    Add files via upload

---
+++ createTable.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect(f'hours.db')
+cur = conn.cursor()
+print("Opened databse successfully")
+
+conn.execute('''CREATE TABLE hours
+        (DATE       DATE     NOT NULL,
+        CATEGORY)           TEXT    NOT NULL,
+        HOURS             INT     NOT NULL);''')
+print("Table created successfully")
